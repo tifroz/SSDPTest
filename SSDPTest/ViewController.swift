49844cc3e33658a9e9b8ce58762e6fe886286b87
tifroz <hhardel@gmail.com>
2022-09-28 12:12:25 -0700

    Initial Commit

---
+++ SSDPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSDPTest
+//
+//  Created by hugo on 9/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
