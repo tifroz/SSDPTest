c8512468630bbc923086566df1ac206432cc46fe
tifroz <hhardel@gmail.com>
2022-09-28 22:32:03 -0700

    Streamlined the code

--- SSDPTest/ViewController.swift
+++ SSDPTest/ViewController.swift
@@ -9,48 +9,32 @@ import UIKit
 import Network
 
 class ViewController: UIViewController {
-
+  
   
   @IBOutlet weak var stateLabel: UILabel!
   
-
+  
+  var connectionGroup: NWMulticastGroup!
   override func viewDidLoad() {
     super.viewDidLoad()
     
     
     let multicastGroup = try! NWMulticastGroup(for: [ .hostPort(host: "239.255.255.250", port: 1900) ])
+    let parameters = NWParameters.udp
+    parameters.allowLocalEndpointReuse = true
+    
     let connectionGroup = NWConnectionGroup(with: multicastGroup, using: .udp)
     stateLabel.text = "\(connectionGroup.state)"
+    
     connectionGroup.setReceiveHandler(maximumMessageSize: 16384, rejectOversizedMessages: true) { message, content, isComplete in
-      NSLog("Received message from \(String(describing: message.remoteEndpoint))")
-      if let content = content, let message = String(data: content, encoding: .utf8) {
-        NSLog("Message: \(message)")
-      }
+      NSLog("Received \(content?.count ?? 0) bytes from \(String(describing: message.remoteEndpoint))")
     }
+    
     connectionGroup.stateUpdateHandler = { newState in
       NSLog("Group entered state \(String(describing: newState))")
-      DispatchQueue.main.async {
-        self.stateLabel.text = "\(newState)"
-      }
+      self.stateLabel.text = "\(newState)"
     }
     
     connectionGroup.start(queue: .main)
-    
-    /*let searchString = "M-SEARCH * HTTP/1.1\r\n" +
-    "HOST: 239.255.255.250:1900\r\n" +
-    "MAN: \"ssdp:discover\"\r\n" +
-    "ST: ssdp:all\r\n" +
-    "MX: 1\r\n\r\n"
-    let groupSendContent = Data(searchString.utf8)
-    connectionGroup.send(content: groupSendContent) { error in
-      if let err = error {
-        NSLog("Send complete with error \(err)")
-      } else {
-        NSLog("Send Complete")
-      }
-    }*/
   }
-
-
 }
-
